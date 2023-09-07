ea15e89441a2f2e4066cb1f39f149ea0c885ef5d
menna020 <mennawork20@gmail.com>
2023-09-07 23:10:15 +0000

    pp

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,4 +1,4 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
@@ -23,4 +23,3 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
