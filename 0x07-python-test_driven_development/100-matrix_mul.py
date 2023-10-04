2941254ac4e19b7f88aa29abc76a68eef39c3c3f
menna020 <mennawork20@gmail.com>
2023-10-04 15:21:47 +0000

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
-# 100-matrix_mul.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a matrix multiplication function."""
+ a matrix multiplication function."""
 
 
 def matrix_mul(m_a, m_b):
