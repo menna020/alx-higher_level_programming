6812acff8df8d078d62c3f643058cd8c20f4b900
menna020 <mennawork20@gmail.com>
2023-10-11 23:41:51 +0000

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a function that reads text files"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """
-    This function reads a text file and prints its contents to stdout.
-
-    Args:
-        filename (str): The name of the file to be read with a default of an empty string.
-
-    Returns:
-        None
-    """
-    with open(filename, encoding="utf-8") as file:
-            print(file.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""The program defines a class student"""
+"""Defines a class Student."""
+
 
 class Student:
-    """Represent a student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """InitializeS a new Student
+        """Initialize a new Student.
 
         Args:
-            first_name (str): The student’s first name
-            last_name (str): The student’s last name
-            age (int): The student’s age
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Gets a dictionary representation of the student"""
+        """Get a dictionary representation of the Student."""
         return self.__dict__
